9aa947348034646c2bf137fc6591a1ff36beeb57
maxmillionsmax <maximilianocaetano@gmail.com>
2022-08-14 02:29:34 -0300

    configurando zuul service

--- src/main/java/com/br/myfood/zuul/ZuulApplication.java
+++ src/main/java/com/br/myfood/zuul/ZuulApplication.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-
+@SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
-@SpringBootApplication
 public class ZuulApplication {
 
 	public static void main(String[] args) {
